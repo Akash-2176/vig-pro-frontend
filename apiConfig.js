47043a4b88f4a7ace125d9e6965eee13d521dbc8
SURENDHAR N D <ndsurendhar2004@gmail.com>
2024-09-02 10:12:20 +0530

    api change1

--- apiConfig.js
+++ apiConfig.js
@@ -1,5 +1,6 @@
 // const API_BASE_URL = "http://localhost:3000/api";
-const API_BASE_URL =
-  "https://vig-pro-backend12-rbmxtoedt-surendhar23s-projects.vercel.app/api";
+// const API_BASE_URL =
+//   "https://vig-pro-backend12-rbmxtoedt-surendhar23s-projects.vercel.app/api";
 // const API_BASE_URL = "http://13.60.216.30:3000/api";
+const API_BASE_URL = "https://backend.vcpoliceportal.in/api";
 export default API_BASE_URL;
