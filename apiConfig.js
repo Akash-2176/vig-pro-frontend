ce37b602c16fce7249c3a0bc6935ae1a40b42c17
SURENDHAR N D <117389583+Surendhar-23@users.noreply.github.com>
2025-09-28 17:24:17 +0530

    Update apiConfig.js

--- apiConfig.js
+++ apiConfig.js
@@ -2,7 +2,9 @@
 // const API_BASE_URL =
 //   "https://vig-pro-backend12-rbmxtoedt-surendhar23s-projects.vercel.app/api";
 // const API_BASE_URL = "http://13.60.233.116/api";
-const API_BASE_URL = "https://backend.vcpoliceportal.in/api";
+// const API_BASE_URL = "https://backend.vcpoliceportal.in/api";
+const API_BASE_URL = "https://vig-pro-backend1.vercel.app/api";
+
 // export API_BASE_URL;
 
 const SOCKET_BASE_URL = "wss://trackingbackend.vcpoliceportal.in";
